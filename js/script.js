8e36d41c1d1ed4807952f9908be91a38fe933b47
RizzedPiecez <2024539699@student.uitm.edu.my>
2025-02-10 21:38:21 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function navigateTo(page) {
+  window.location.href = page;
+}
+
+<section class="text-box" id="text-box">
+  Welcome to Page 3! This is a test message inside the neon green text box.
+</section>
+
